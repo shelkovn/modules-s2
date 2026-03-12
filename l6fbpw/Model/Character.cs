3968486330d650da89eb8dcb0bcef28e98e82ecc
towa <309>
2026-03-12 15:53:33 +0700

    flyweight

---
+++ l6fbpw/Model/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l6fbpw.Model
+{
+    public class Character
+    {
+        char _symbol;
+        string _font; 
+        int _fontSize;
+
+        public Character(char c, string font, int size)
+        {
+            _symbol = c;
+            _font = font;
+            _fontSize = size;
+        }
+
+        public void Draw(int x, int y)
+        {
+            Console.WriteLine($"{_symbol} {x} {y}");
+        }
+    }
+}
---
+++ l6fbpw/Model/CharacterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l6fbpw.Model
+{
+    public class CharacterFactory
+    {
+        private Dictionary<string, Character> _characters = new();
+        public Character GetCharacter(char c, string font, int fontsize)
+        {
+            string key = $"{font}_{fontsize}_{c}";
+            if (!_characters.ContainsKey(key))
+            {
+                _characters[key] = new Character(c, font, fontsize);
+                Console.WriteLine($"Creating new character: {key}");
+            }
+            return _characters[key];
+        }
+        public int GetCount() => _characters.Count;
+    }
+}
