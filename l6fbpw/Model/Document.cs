cad4373c999d758fae5eb45684c41a05362c2a19
towa <..>
2026-03-14 13:35:02 +0700

    page document

---
+++ l6fbpw/Model/Document.cs
@@ -0,0 +1,28 @@
+﻿namespace l6fbpw.Model
+{
+    public class Document
+    {
+        private List<Page> _pages = new List<Page>();
+        private IRenderingEngine _engine;
+        public Document(IRenderingEngine engine)
+        {
+            _engine = engine;
+        }
+        public Page CreatePage()
+        {
+            var page = new Page();
+            _pages.Add(page);
+            return page;
+        }
+        public void RenderAll()
+        {
+            _engine.BeginRender();
+            for (int i = 0; i < _pages.Count; i++)
+            {
+                Console.WriteLine($"\n--- Страница {i + 1} ---");
+                _pages[i].Render();
+            }
+            _engine.EndRender();
+        }
+    }
+}
