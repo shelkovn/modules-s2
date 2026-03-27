07d600aa6aabfb2fd6504e509c1cfa37d0577afa
towa <..>
2026-03-27 18:22:24 +0700

    strategy + handlerbase

---
+++ l7behpat/Model/IFormatStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace l7behpat.Model
+{
+    public interface IFormatStrategy
+    {
+        string Format(string message, DateTime timestamp);
+    }
+}
---
+++ l7behpat/Model/JsonFormatStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace l7behpat.Model
+{
+    public class JsonFormatStrategy : IFormatStrategy
+    {
+        public string Format(string message, DateTime timestamp) =>
+        $"{{\"timestamp\":\"{timestamp:O}\",\"message\":\"{message}\"}}";
+    }
+}
