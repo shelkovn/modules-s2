03e1c0aebb170e5ff0780ab9a6ef516a63511a58
towa <..>
2026-03-27 19:30:46 +0700

    test

--- l7behpat/Program.cs
+++ l7behpat/Program.cs
@@ -9,6 +9,12 @@ namespace l7behpat
             EventMonitor jajah = new EventMonitor();
             jajah.OnMetricExceeded += e => Console.WriteLine("ну че там твин получилось проц разогнать?");
             jajah.CheckMetric();
+            JsonFormatStrategy json = new JsonFormatStrategy();
+            TextFormatStrategy text = new TextFormatStrategy();
+            ConsoleHandler console = new ConsoleHandler(json);
+            FileHandler file = new FileHandler(text);
+            file.ProcessEvent(new MetricEventArgs("rat", new MetricData("network", 1000, 100, DateTime.Now)));
+            console.ProcessEvent(new MetricEventArgs("matgorit", new MetricData("temp", 250, 100, DateTime.Now)));
         }
     }
 }
