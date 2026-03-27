dcffeda75e8b78d6b71fad9eeedd02529fa1c3f8
towa <..>
2026-03-27 17:31:33 +0700

    observer

---
+++ l7behpat/Program.cs
@@ -0,0 +1,14 @@
+﻿using l7behpat.Model;
+
+namespace l7behpat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventMonitor jajah = new EventMonitor();
+            jajah.OnMetricExceeded += e => Console.WriteLine("ну че там твин получилось проц разогнать?");
+            jajah.CheckMetric();
+        }
+    }
+}
